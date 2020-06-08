6c0dca50dcdc74273d0e1cf087d3f48877f310a6
IgorVerchyk <sabyr.ivua@gmail.com>
2020-06-08 18:05:25 +0300

    6_task_fixed

--- task-06.js
+++ task-06.js
@@ -1,16 +1,15 @@
 const validatedPassword = document.querySelector("#validation-input");
 validatedPassword.addEventListener("blur", checkForValid);
 function checkForValid(event) {
-  if (validatedPassword.value.length === 0) {
+  const passwordInput = validatedPassword.value.length;
+  const passwordLenth = Number(validatedPassword.dataset.length);
+  if (passwordInput === 0) {
     validatedPassword.classList.remove("valid", "invalid");
-  } else if (
-    validatedPassword.value.length == validatedPassword.dataset.length
-  ) {
-    validatedPassword.removeAttribute("style");
+  } else if (passwordInput === passwordLenth) {
     validatedPassword.classList.add("valid");
+    validatedPassword.classList.remove("invalid");
   } else {
-    validatedPassword.removeAttribute("style");
     validatedPassword.classList.add("invalid");
+    validatedPassword.classList.remove("valid");
   }
-  console.dir(validatedPassword);
 }
